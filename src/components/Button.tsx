95ba61f9aadec43ee9c080e629de1b323760bc02
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-06-22 00:12:17 -0300

    criacao da pagina inicial, criacao de sala e autenticacao google

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react"
+import {ButtonHTMLAttributes} from 'react'
 
-export function Button() {
+import '../styles/button.scss'
 
-  const [counter, setCounter] = useState(0)
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-  function increment(){
-    setCounter(counter+1);
-  }
+export function Button(props:ButtonProps) {
 
   return (
-    <button onClick={increment}>{counter}</button>
+    <button className="button" {...props}></button>
   )
 }
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import { useAuth } from '../hooks/useAuth';
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+export function NewRoom() {
+
+const { user } = useAuth();
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main className="main-content">
+        <img src={logoImg} alt="Logo da aplicação Letmeask" />
+        <h1>{user?.name}</h1>
+        <h2>Criar uma nova sala</h2>
+        <form action="">
+          <input
+            type="text"
+            placeholder="Nome da sala"
+          />
+          <Button type="submit">
+            Criar sala
+          </Button>
+
+        </form>
+        <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+      </main>
+    </div>
+  )
+}
